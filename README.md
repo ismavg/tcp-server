0b2f34b20247786afd1747e4326d8a076937ccb7
ismavg <ismavg.krishna@gmail.com>
2021-07-12 18:44:54 +0530

    bridge option from port1 to port2

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # tcp-server
-Test server using nodeJS
+Test server using nodeJS with ECHO and bridge options
+
+ENV config available
+PORT1 default 9000
+PORT2 default 9001
+ECHO default false (data is ECHO the client if true)
+BRIDGE default false (data is bridged between PORT1 and PORT2 if true)
--- server.js
+++ server.js
@@ -1,17 +1,46 @@
 'use strict';
 
+const { Socket } = require('dgram');
 const net = require('net');
-global.PORT = process.env.PORT || 9000;
+global.PORT1 = process.env.PORT1 || 9000;
+global.PORT2 = process.env.PORT2 || 9001;
 global.ECHO = (process.env.ECHO == 'true' ? true:false);
-const server = net.createServer((socket) => {
+global.BRIDGE = (process.env.BRIDGE === 'true' ? true:false);
+var socket1 = null; 
+var socket2 = null;
+
+const server1 = net.createServer((socket) => {
+	socket1 = socket;
 	socket.on('data', (received_data) => {
-		console.log('> ', received_data.toString());
+		console.log('CS1R> ', received_data.toString());
+		if(socket2!=null && BRIDGE){
+			socket2.write(received_data);
+		}
 		if(ECHO) {
 			socket.write(received_data);
 		}
 	});
 });
 
-server.listen(PORT, () => {
-	console.log('Datoms test communication tcp server started and bound to ' + PORT);
+
+server1.listen(PORT1, () => {
+	console.log('Datoms test communication tcp server started and bound to ' + PORT1);
 });
+
+const server2 = net.createServer((socket) => {
+	socket2 = socket;
+	socket.on('data', (received_data) => {
+		console.log('CS2R> ', received_data.toString());
+		if(socket1 != null && BRIDGE){
+			socket1.write(received_data);
+		}
+		if(ECHO) {
+			socket.write(received_data);
+		}
+	});
+});
+
+server2.listen(PORT2, () => {
+	console.log('Datoms test communication tcp server started and bound to ' + PORT2);
+});
+  
